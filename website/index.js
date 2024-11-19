2d1e9926e493a3a8511987d7526f78e36f6b24f8
gerkki <gustaf.erkki@gmail.com>
2024-11-19 20:55:48 +0100

    Skapande av funktionen "lookForCity"
    La till en funktion som försöker matcha prompten med en stad i arrayen. La även in så att detta uppdateras i h2-taggen.

--- website/index.js
+++ website/index.js
@@ -1,6 +1,13 @@
 // Recommended: All functions declared here
 
-
+function lookForCity(targetCityName) {
+    for (let city of cities) {
+        if (city.name == targetCityName) {
+            return city;
+        }
+    }
+    return null;
+}
 
 // Recommended: constants with references to existing HTML-elements
 
@@ -11,6 +18,14 @@ const citiesDiv = document.getElementById("cities");
 // Recommended: Ask for the city name and then the rest of the code
 
 const targetCityName = prompt("Vilken stad?");
+
+const foundCity = lookForCity(targetCityName);
+
+if (foundCity == null) {
+    h2.textContent = `${targetCityName} finns inte i databasen`;
+} else {
+    h2.textContent = `${foundCity.name} (${foundCity.country})`;
+}
 
 for (let city of cities) {
     citiesDiv.innerHTML += `<p class="cityBox">${city.name}</p>`;
