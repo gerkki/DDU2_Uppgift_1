a65138dd96efcb10cf1bdc800cd5aeb886aa2fbb
gerkki <gustaf.erkki@gmail.com>
2024-11-19 13:29:58 +0100

    Konstanter, länkning till databas & städer
    Skapade de första konstanterna och länkade database.js till index.html. La även in en for of-loop för att lista upp alla städerna på sidan.

--- website/index.js
+++ website/index.js
@@ -1,6 +1,17 @@
 // Recommended: All functions declared here
 
+
+
 // Recommended: constants with references to existing HTML-elements
+
+const h2 = document.querySelector("h2");
+const h3 = document.querySelector("h3");
+const citiesDiv = document.getElementById("cities");
 
 // Recommended: Ask for the city name and then the rest of the code
 
+const targetCityName = prompt("Vilken stad?");
+
+for (let city of cities) {
+    citiesDiv.innerHTML += `<p class="cityBox">${city.name}</p>`;
+}
