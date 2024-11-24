1baeb51a65293afd12216e1960386e67eab64016
gerkki <gustaf.erkki@gmail.com>
2024-11-24 12:53:31 +0100

    Strukturerat om och skapat funktioner
    Gjorde ändringar i kodens struktur. Tog fram en function som skapar städerna, samt en function som tilldelar olika färger till städerna

--- website/index.js
+++ website/index.js
@@ -1,5 +1,14 @@
 // Recommended: All functions declared here
 
+function createAllCityBoxes() {
+    for (let city of cities) {
+        const cityElement = document.createElement("p");
+        cityElement.textContent = city.name;
+        cityElement.classList.add("cityBox");
+        citiesDiv.appendChild(cityElement);
+    }
+}
+
 function lookForCity(targetCityName) {
     for (let city of cities) {
         if (city.name == targetCityName) {
@@ -7,6 +16,15 @@ function lookForCity(targetCityName) {
         }
     }
     return null;
+}
+
+function markCityBox(cityObject, kindOfCity) {
+    const cityElements = document.querySelectorAll(".cityBox");
+    for (let cityElement of cityElements) {
+        if (cityElement.textContent === cityObject.name) {
+            cityElement.classList.add(kindOfCity);
+        }
+    }
 }
 
 // Recommended: constants with references to existing HTML-elements
@@ -18,6 +36,10 @@ const tableDiv = document.getElementById("table");
 
 // Recommended: Ask for the city name and then the rest of the code
 
+const allCities = createAllCityBoxes();
+
+// const completeTable = Här ska funktionen för tabellen anropas!
+
 const targetCityName = prompt("Vilken stad?");
 
 const foundCity = lookForCity(targetCityName);
@@ -26,19 +48,13 @@ if (foundCity == null) {
     h2.textContent = `${targetCityName} finns inte i databasen`;
     h3.textContent = "";
     document.title = "Not Found";
-} else {
+}
+
+if (foundCity !== null) {
     h2.textContent = `${foundCity.name} (${foundCity.country})`;
     document.title = foundCity.name;
+    markCityBox(foundCity, "target");
+    h3.textContent = `Av städerna i databasen ligger x närmast och y längst bort`;
 }
 
-for (let city of cities) {
-    const cityElement = document.createElement("p");
-    cityElement.classList.add("cityBox");
 
-    if (city.name == targetCityName) {
-        cityElement.classList.add("target");
-    }
-
-    cityElement.textContent = city.name;
-    citiesDiv.appendChild(cityElement);
-}
